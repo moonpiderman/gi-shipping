0ad30cd8a4306a3bb8531883ea1f93a429c8b8dc
moonpiderman <lotsofchuck@gmail.com>
2023-08-01 11:25:34 +0900

    first commit

---
+++ src/main/kotlin/jiujitsu/gi/shoyoroll/ShoyorollApplication.kt
@@ -0,0 +1,11 @@
+package jiujitsu.gi.shoyoroll
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShoyorollApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShoyorollApplication>(*args)
+}
---
+++ src/test/kotlin/jiujitsu/gi/shoyoroll/ShoyorollApplicationTests.kt
@@ -0,0 +1,13 @@
+package jiujitsu.gi.shoyoroll
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShoyorollApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
